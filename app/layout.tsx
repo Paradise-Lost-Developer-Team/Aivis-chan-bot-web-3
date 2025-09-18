6966c1379940451a069910b57994577675a7c2da
Yui Tanabe <walker27460@users.noreply.github.com>
2025-09-18 14:29:06 +0800

    feat: change ;ayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
-import { Html } from "next/document";
 import { Provider } from "../components/provider"
 
 export default function RootLayout({ children }) {
   return (
-    <Html>
+    <html>
       <body>
         <Provider>
           {children}
         </Provider>
       </body>
-    </Html>
+    </html>
   )
 }
